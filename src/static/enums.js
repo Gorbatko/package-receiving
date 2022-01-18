4c1b86242733fa39f5918705d34b7a523231d536
Sergey Gorbatko <gorbatko.ser@gmail.com>
2022-01-18 18:58:00 +0300

    Add weights and sizes variants

--- src/static/enums.js
+++ src/static/enums.js
@@ -1,4 +1,4 @@
-const weights = ['<1kg', '<2kg', '<5kg', '>5kg'];
-const sizes = ['12" x 8" x 8"', '12" x 12" x 18"', '24" x 24" x 24"'];
+const weights = ['<1kg', '<2kg', '<5kg', '<10kg', '>10kg'];
+const sizes = ['6" x 4" x 2"', '12" x 8" x 8"', '12" x 12" x 18"', '24" x 24" x 24"', '32" x 28" x 26"'];
 
 export { weights, sizes };
